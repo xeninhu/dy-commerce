989f269c0a78e78c297ebfb2657c74c72d3c09cf
Diogenes Duarte <diogenes.duarte@gmail.com>
2014-05-29 01:28:49 -0300

    Mudança para toda vez que pedir um entityManager verificar se o atual já foi fechado, e caso tenha sido, criar outro...

--- classes/Config/bootstrap.php
+++ classes/Config/bootstrap.php
@@ -6,26 +6,28 @@
 	
 	/**
 	 * Classe para recuperar o entity manager do doctrine 2.
-	 * Cada entity vai para a sessão do usuário para que não seja necessário criar novamente.
-	 * Não é um singleton para todas as requisições para não ter problema de concorrência nas transações.
+	 * A instancia da classe vai para a sessão do usuário para que não seja necessário criar novamente.
+	 * Não é um singleton para não ter problema de concorrência nas transações.
 	 *
 	 */
 	Class Bootstrap {
+		private $config;
+		private $params;
 		private $entity_manager;
 		
 		private function __construct() {
 			$entidades = array("classes/entities");
 			$isDevMode = true;
 
-			$dbParams = array(
+			$this->params = array(
 			    'driver'   => 'pdo_mysql',
 			    'user'     => 'root',
 			    'password' => '',
 			    'dbname'   => 'dycommerce',
 			);
 			
-			$config = Setup::createAnnotationMetadataConfiguration($entidades, $isDevMode);
-			$this->entity_manager = EntityManager::create($dbParams, $config);
+			$this->config = Setup::createAnnotationMetadataConfiguration($entidades, $isDevMode);
+			$this->entity_manager = EntityManager::create($this->params, $this->config);
 			$_SESSION['bootstrap'] = $this;
 		}
 		
@@ -38,7 +40,14 @@
 			return $_SESSION['bootstrap']; 
 		}
 		
+		/**
+		 * Verifica se o entityManager já foi utilizado e fechado, e caso isso tenha ocorrido, descarto
+		 * e crio outro para enviar ao usuário
+		 */
 		public function get_entity_manager() {
+			if(!$this->entity_manager->isOpen()) {
+				$this->entity_manager = EntityManager::create($this->params, $this->config);
+			}
 			return $this->entity_manager;
 		}
 	}
